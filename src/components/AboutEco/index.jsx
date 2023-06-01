67dde414537ccd24d5dc10b9fa4fe30c42ecbae9
JulioCDeveloper <julioc.developer@gmail.com>
2023-06-01 18:46:08 -0400

    twste

--- src/components/AboutEco/index.jsx
+++ src/components/AboutEco/index.jsx
@@ -4,7 +4,6 @@ import { Box, Typography } from '@mui/material'
 
 const AboutEco = () => {
     return (
-
         <section class="text-gray-600 body-font">
             <div class="container mx-auto flex px-5  md:flex-row flex-col items-center">
                 <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
